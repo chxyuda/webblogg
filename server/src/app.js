8846ff149378a41627d2e3eed2cee75cfbd75b4c
chxyuda <kawneaw27@gmail.com>
2023-10-03 11:51:42 +0700

    page147

--- server/src/app.js
+++ server/src/app.js
@@ -1,55 +1,52 @@
-let express = require('express')
-let bodyParser = require('body-parser')
+let express = require('express');
+let bodyParser = require('body-parser');
+let cors = require('cors')
 const {sequelize} = require('./models')
 
 const config = require('./config/config')
 
-const app = express()
- 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+const app = express();
 
-require('./routes')(app)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
-app.get('/status', function (req,res){
-    res.send('Hello nodejs server belong to Chayuda')
-})
+require('./route')(app)
 
-app.get('/hello/:name', function (req,res) {
-    console.log('hello - ' + req.params.name)
-    res.send('sey hello with ' + req.params.name)
-   })
+// app.get('/status', function (req, res) {
+//     res.send('Hello nodejs server belonging to nitiphumi');
+// });
 
-   app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน'+ req.params.userId)
-   })
+// app.get('/hello/:name', function (req, res) {
+//     console.log('hello - ' + req.params.name);
+//     res.send('Say hello with ' + req.params.name);
+// });
 
-   app.get('/user/:users', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน')
-   })
+// app.get('/user/:userId', function (req, res) {
+//     res.send('ดูข้อมูลผู้ใช้งาน' + req.params.userId);
+// });
 
-   app.post('/user/', function (req, res) {
-    res.send('สร้างผู้ใช้งาน' + JSON.stringify(req.body))
-   })
+// app.get('/users', function (req, res) {
+//     res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด');
+// });
 
-   app.put('/user/:userId', function (req, res) {
-    res.send('แก้ไขข้อมูลผู้ใช้' + req.params.userId + ' : ' +
-   JSON.stringify(req.body.name))
-   })
+// app.post('/user/:userId', function (req, res) {
+//     res.send('ทำการสร้างผู้ใช้งาน ' + JSON.stringify(req.body));
+// });
 
-   app.delete('/user/:userId', function (req, res) {
-    res.send('ลบผุ้ใช้งาน' + req.params.userId + ' : ' + 
-    JSON.stringify(req.body.name))
-})
+// app.put('/user/:userId', function (req, res) {
+//     res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+// });
 
-let port = process.env.PORT || config.port
+// app.delete('/user/:userId', function (req, res) {
+//     res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body));
+// });
+
+let port = process.env.PORT || config.port;
 
 sequelize.sync({force: false}).then(() => {
-    app.listen(port, function () {
-    console.log('Server running on ' + port)
-    })
+ app.listen(port, function () {
+ console.log('Server running on ' + port)
+ })
 })
 
-app.listen(port, function(){
-    console.log('server running on ' + port)
-})
--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,24 +1,83 @@
-module.exports = {
-// get all user
-    index(req, res) {
-        res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
-       },
+const { User } = require('../models')
 
-// create user
-       create(req, res) {
-        res.send('สร้างผู้ใช้งาน' + JSON.stringify(req.body))
-       },
-// edit user, suspend, active
-       put(req, res) {
-        res.send('แก้ไขข้อมูลผู้ใช้' + req.params.userId + " : " +JSON.stringify(req.body.name))
-       },
-// delete user
-       delete(req, res) {
-    res.send('ลบข้อมูลผุ้ใช้' + req.params.userId + " : " + JSON.stringify(req.body.name))
+module.exports = {
+
+    // Get all users
+    async index(req, res) {
+        try {
+            const users = await User.findAll()
+            res.send(users)
+        } catch (err) {
+            res.status(500).send({
+                error: 'The users information was incorrect'
+            })
+        }
     },
-// get user by id
-     show (req, res) {
-       res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId + " : " + JSON.stringify(req.body))
+    // Create user
+    // res.send('ทำการสร้างผู้ใช้งาน' + JSON.stringify(req.body));
+    async create(req, res) {
+        try {
+            const user = await User.create(req.body)
+            res.send(user.toJSON())
+        } catch (err) {
+            res.status(500).send({
+                error: 'Create user incorrect'
+            })
+        }
+    },
+
+    // Edit user
+    // res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+    async put(req, res) {
+        try {
+            await User.update(req.body, {
+                where: {
+                    id: req.params.userId
+                }
+            })
+            res.send(req.body)
+        } catch (err) {
+            res.status(500).send({
+                error: 'Update user incorrect'
+            })
+        }
+    },
+
+    // Delete user
+    // res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' + JSON.stringify(req.body));
+    // delete user
+    async remove(req, res) {
+        try {
+            const user = await User.findOne({
+                where: {
+                    id: req.params.userId
+                }
+            })
+            if (!user) {
+                return res.status(403).send({
+                    error: 'The user information was incorrect'
+                })
+            }
+            await user.destroy()
+            res.send(user)
+        } catch (err) {
+            res.status(500).send({
+                error: 'The user information was incorrect'
+            })
+        }
+    },
+
+    // Show user by id
+    async show(req, res) {
+        try {
+            const user = await User.findByPk(req.params.userId)
+            res.send(user)
+        } catch (err) {
+            res.status(500).send({
+                error: 'The user information was incorrect'
+            })
+        }
     }
-   }
+}
+
 
